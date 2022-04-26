43536ba435da8fb0eacd835a734c688fe0bc371c
srlavelle <72841482+srlavelle@users.noreply.github.com>
2022-04-26 14:59:58 -0400

    Cleaned up, formatted output, added puzzles
    Cleaned up code and added comments describing helper functions.
    Formatted the output to be compatible with 3-digit numbers, and just look clearer visually.
    Added puzzles: 9x9 from Numbrix website & 15x15, the largest I could find.
    Added code to time puzzle-solving.
    
    Even the 15x15 is taking only 0.5 ms, so I'm concerned about speedup - overhead may be significantly larger than this. I think I'll try OpenMP, but others may have less overhead.

--- numbrix_main.cpp
+++ numbrix_main.cpp
@@ -7,6 +7,7 @@
 #include <cstdlib>
 #include <string>
 #include <bitset>
+#include <chrono>
 
 using namespace std;
 typedef bitset<4> hood_t;
@@ -28,10 +29,10 @@ public:
 		hei = static_cast<int>(puzz.size()) / wid;
 		max = wid * hei;
 		int len = max, c = 0;
-		arr = vector<node>(len, node({ 0, 0 }));
-		weHave = vector<bool>(len + 1, false);
+		arr = vector<node>(len, node({ 0, 0 })); // holds solution
+		weHave = vector<bool>(len + 1, false); // hold whether or not that position is solved
 
-		for (const auto& s : puzz)
+		for (const auto& s : puzz) // go thru and mark what values are already there
 		{
 			if (s == "*") { max--; arr[c++].val = -1; continue; }
 			arr[c].val = atoi(s.c_str());
@@ -39,8 +40,9 @@ public:
 			c++;
 		}
 
-		solveIt(); c = 0;
-		for (auto&& s : puzz)
+		solveIt(); // does the solving
+        c = 0;
+		for (auto&& s : puzz) // fill in all the unknowns from the solution in arr
 		{
 			if (s == ".")
 				s = std::to_string(arr[c].val);
@@ -93,7 +95,7 @@ private:
 		for (int xx = 0; xx < 4; xx++)
 		{
 			int a = x + dx[xx], b = y + dy[xx];
-			if (a < 0 || b < 0 || a >= wid || b >= hei)
+			if (a < 0 || b < 0 || a >= wid || b >= hei) // if out of bounds of grid
 				continue;
 			if (arr[a + b * wid].val > -1)
 				retval.set(xx);
@@ -103,12 +105,15 @@ private:
 
 	void solveIt()
 	{
-		int x, y, z; findStart(x, y, z);
+		int x, y, z;
+        findStart(x, y, z); // set x & y to coords of lowest value. z holds this value
 		if (z == 99999) { cout << "\nCan't find start point!\n"; return; }
-		search(x, y, z + 1, 1);
-		if (z > 1) search(x, y, z - 1, -1);
+		search(x, y, z + 1, 1); // recursively finds all numbers > z
+		if (z > 1) search(x, y, z - 1, -1); // recursively finds all numbers < z
 	}
 
+    // goes through all values in puzzle to find lowest value.
+    // this is the starting point.
 	void findStart(int& x, int& y, int& z)
 	{
 		z = 99999;
@@ -132,12 +137,84 @@ private:
 //------------------------------------------------------------------------------
 int main(int argc, char* argv[])
 {
-	int wid; string p;
-	//p = ". . . . . . . . . . . 46 45 . 55 74 . . . 38 . . 43 . . 78 . . 35 . . . . . 71 . . . 33 . . . 59 . . . 17 . . . . . 67 . . 18 . . 11 . . 64 . . . 24 21 . 1  2 . . . . . . . . . . ."; wid = 9;
-	//p = ". . . . . . . . . . 11 12 15 18 21 62 61 . .  6 . . . . . 60 . . 33 . . . . . 57 . . 32 . . . . . 56 . . 37 .  1 . . . 73 . . 38 . . . . . 72 . . 43 44 47 48 51 76 77 . . . . . . . . . ."; wid = 9;
-	p = "17 . . . 11 . . . 59 . 15 . . 6 . . 61 . . . 3 . . .  63 . . . . . . 66 . . . . 23 24 . 68 67 78 . 54 55 . . . . 72 . . . . . . 35 . . . 49 . . . 29 . . 40 . . 47 . 31 . . . 39 . . . 45"; wid = 9;
+    // timing code modeled after that for Assignment 3
+    using namespace std::chrono;
+    typedef std::chrono::high_resolution_clock Clock;
+    typedef std::chrono::duration<double> dsec;
 
-	istringstream iss(p); vector<string> puzz;
+	int wid;
+    string p;
+    string p1;
+    string p2;
+    string p3;
+    string p4;
+    string p5;
+
+
+	p1 =".  .  .  .  .  .  .  .  . \
+         .  . 46 45  . 55 74  .  . \
+         . 38  .  . 43  .  . 78  . \
+         . 35  .  .  .  .  . 71  . \
+         .  . 33  .  .  . 59  .  . \
+         . 17  .  .  .  .  . 67  . \
+         . 18  .  . 11  .  . 64  . \
+         .  . 24 21  .  1  2  .  . \
+         .  .  .  .  .  .  .  .  ."; int wid1 = 9;
+
+    p2 =".  .  .  .  .  .  .  .  . \
+         . 11 12 15 18 21 62 61  . \
+         .  6  .  .  .  .  . 60  . \
+         . 33  .  .  .  .  . 57  . \
+         . 32  .  .  .  .  . 56  . \
+         . 37  .  1  .  .  . 73  . \
+         . 38  .  .  .  .  . 72  . \
+         . 43 44 47 48 51 76 77  . \
+         .  .  .  .  .  .  .  .  ."; int wid2 = 9;
+
+    p3 ="17  .  .  . 11  .  .  . 59 \
+          . 15  .  .  6  .  . 61  . \
+          .  .  3  .  .  . 63  .  . \
+          .  .  .  . 66  .  .  .  . \
+         23 24  . 68 67 78  . 54 55 \
+          .  .  .  . 72  .  .  .  . \
+          .  . 35  .  .  . 49  .  . \
+          . 29  .  . 40  .  . 47  . \
+         31  .  .  . 39  .  .  . 45"; int wid3 = 9;
+
+    // This is the example puzzle from the Numbrix website!
+    p4 =" .  .  .  .  .  .  .  .  . \
+          . 25  . 31  . 69  . 73  . \
+          .  . 19  . 35  . 79  .  . \
+          . 21  .  .  .  .  . 61  . \
+          .  . 15  .  .  . 81  .  . \
+          .  9  .  .  .  .  . 63  . \
+          .  .  7  . 45  . 51  .  . \
+          .  1  . 41  . 47  . 53  . \
+          .  .  .  .  .  .  .  .  ."; int wid4 = 9;
+
+    p5 ="53  52   .   .  47   .   .   .   .   . 211   .   . 202 201 \
+         54  51   .   .   .   .   .  21   .   .   .   .   . 203 200 \
+          .   .   .  44   .   .   .   .   .   .   . 216   .   .   . \
+          .   .  60   .   .  39   .  23   . 219   .   . 206   .   . \
+         63   .   .   .   .  40  29  24  15  10   .   .   .   . 197 \
+          .   .   .  69  70  71   .   .   .  11   8 225   .   .   . \
+          .   .   .   .  73   .  27   .  13   .   7   .   1   .   . \
+          .  77   .  83  84   .   .  89   .   .  92   5   . 191   . \
+          .   .   .   .  85   .  99   .  95   .  93   .   .   .   . \
+          .   .   . 119 118 101   .   .   . 165 166 169   .   .   . \
+        129   .   .   .   . 102 103 104 105 164   .   .   .   . 185 \
+          .   . 124   .   . 115   . 111   . 163   .   . 172   .   . \
+          .   .   . 142   .   .   .   .   .   .   . 160   .   .   . \
+        136 135   .   .   .   .   . 109   .   .   .   .   . 179 178 \
+        137 138   .   . 145   .   .   .   .   . 153   .   . 176 177"; int wid5 = 15;
+
+
+    // choose puzzle to solve
+    p = p1; // maybe make command-line arg later
+    wid = wid1;
+
+    istringstream iss(p);
+    vector<string> puzz;
 	copy(istream_iterator<string>(iss), istream_iterator<string>(), back_inserter<vector<string> >(puzz));
 
     // first print unsolved puzzle
@@ -146,31 +223,42 @@ int main(int argc, char* argv[])
 	{
 		if (s != "*" && s != ".")
 		{
-			if (atoi(s.c_str()) < 10) cout << "0";
+			if (atoi(s.c_str()) < 10) cout << " ";
+            if (atoi(s.c_str()) < 100) cout << " ";
 			cout << s << " ";
 		}
-		else cout << "   ";
+		else cout << "___ ";
 		if (++c >= wid) { cout << endl; c = 0; }
 	}
-	cout << endl << endl;
+	cout << endl;
 
 
     nSolver s;
+
+    // start time
+    auto compute_start = Clock::now();
+    double compute_time = 0;
+
     s.solve(puzz, wid);
+    // end time
+    compute_time += duration_cast<dsec>(Clock::now() - compute_start).count();
+
+    cout << "Solved!\n";
+    cout << "Computation time: " << compute_time*1000 << " ms\n\n";
 
     // print solved puzzle
-	c = 0; // tracks position in row
+	c = 0;
 	for (const auto& s : puzz)
 	{
 		if (s != "*" && s != ".")
 		{
-			if (atoi(s.c_str()) < 10) cout << "0";
+			if (atoi(s.c_str()) < 10) cout << " ";
+            if (atoi(s.c_str()) < 100) cout << " ";
 			cout << s << " ";
 		}
 		else cout << "   ";
 		if (++c >= wid) { cout << endl; c = 0; }
 	}
-	cout << endl << endl;
 
     return 1; // indicating success
 }
